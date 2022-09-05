78c27a8ce80e5657c69f688fe6a5dc7896ff15be
KutayDemiray <kutaydemiray921@gmail.com>
2022-09-05 16:42:49 +0300

    Added "\n" and "\t" support on terminal

--- balbaros/kernel/kernel/kernel.c
+++ balbaros/kernel/kernel/kernel.c
@@ -6,7 +6,7 @@
 
 void kernel_main(void) {
   terminal_initialize();
-  printf("Hello, kernel World!\n");
+  printf("Hello, kernel World!\nThis \t is \t a \t line \t with \t some \t\t tabs \t\t inbetween \t!");
 #ifdef __RUN_TESTS
   run_tests();
   printf("Test phase over\n");
